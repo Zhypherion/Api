cb938f6e7bd3b519f272799e6b518e0a5971d80b
Zhypherion <luckybow1232@gmail.com>
2025-10-03 18:08:34 +0800

    Midway Deployment v2.5

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
+// config.js
 module.exports = {
   database: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'node-mysql-signup-verification-api-latest',
+    host: process.env.MYSQLHOST || 'localhost',
+    port: process.env.MYSQLPORT || 25699,
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || 'root',
+    database: process.env.MYSQLDATABASE || 'node-mysql-signup-verification-api-latest',
   },
   secret: process.env.JWT_SECRET || '280062ee-c5f2-43d1-bafb-ecf1f7901952',
   emailFrom: process.env.EMAIL_FROM || 'info@node-mysql-signup-verification-api.com',
