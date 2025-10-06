62f126bd5e8a0a78448b90a3eb606e536fb6fe09
Zhypherion <luckybow1232@gmail.com>
2025-10-06 11:47:59 +0800

    Added changes to store refresh token in server and accounts.controller

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+app.use(cors({ origin: (origin, callback) => callback(null, true), origin: ['https://frontend-seven-eta-11.vercel.app'], credentials: true }));
 
 // api routes
 app.use('/accounts', require('./accounts/accounts.controller'));
