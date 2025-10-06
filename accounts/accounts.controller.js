9a226d76d97f597ac16f09f8d687596e042878dd
Zhypherion <luckybow1232@gmail.com>
2025-10-06 11:58:48 +0800

    Revert changes

--- accounts/accounts.controller.js
+++ accounts/accounts.controller.js
@@ -251,8 +251,6 @@ function setTokenCookie(res, token) {
     // create cookie with refresh token that expires in 7 days
     const cookieOptions = {
         httpOnly: true,
-        sameSite: 'none',//added after deployed
-        secure: true,//added after deployed
         expires: new Date(Date.now() + 7*24*60*60*1000)
     };
     res.cookie('refreshToken', token, cookieOptions);
