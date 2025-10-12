185ebf0e89df543ace078c38cd9f42f219b8ecfd
Zhypherion <luckybow1232@gmail.com>
2025-10-12 17:07:06 +0800

    changed getbyid in position.service to include department id,name

--- positions/position.service.js
+++ positions/position.service.js
@@ -1,3 +1,55 @@
+// const db = require('_helpers/db');
+
+// module.exports = {
+//   getAll,
+//   getById,
+//   create,
+//   update,
+//   delete: _delete
+// };
+
+// async function getAll() {
+//   return await db.Position.findAll({
+//     include: [
+//       {
+//         model: db.Department,
+//         as: 'department', // alias (must match your association)
+//         attributes: ['id', 'name']
+//       }
+//     ],
+//     order: [['hierarchyLevel', 'ASC']]
+//   });
+// }
+
+// async function getById(id) {
+//   return await db.Position.findByPk(id);
+// }
+
+// async function create(params) {
+//   return await db.Position.create(params);
+// }
+
+// async function update(id, params) {
+//   const position = await getById(id);
+//   if (!position) throw 'Position not found';
+//   Object.assign(position, params);
+//   await position.save();
+//   return position;
+// }
+
+// async function _delete(id) {
+//   const position = await getById(id);
+//   if (!position) throw 'Position not found';
+//   await position.destroy();
+// }
+
+
+
+
+
+
+
+
 const db = require('_helpers/db');
 
 module.exports = {
@@ -13,7 +65,7 @@ async function getAll() {
     include: [
       {
         model: db.Department,
-        as: 'department', // alias (must match your association)
+        as: 'department', // must match association alias
         attributes: ['id', 'name']
       }
     ],
@@ -22,11 +74,23 @@ async function getAll() {
 }
 
 async function getById(id) {
-  return await db.Position.findByPk(id);
+  return await db.Position.findByPk(id, {
+    include: [
+      {
+        model: db.Department,
+        as: 'department',
+        attributes: ['id', 'name']
+      }
+    ]
+  });
 }
 
 async function create(params) {
-  return await db.Position.create(params);
+  const position = await db.Position.create(params);
+  // Reload it with department data before returning
+  return await db.Position.findByPk(position.id, {
+    include: [{ model: db.Department, as: 'department', attributes: ['id', 'name'] }]
+  });
 }
 
 async function update(id, params) {
@@ -34,7 +98,7 @@ async function update(id, params) {
   if (!position) throw 'Position not found';
   Object.assign(position, params);
   await position.save();
-  return position;
+  return await getById(id); // return updated position with department info
 }
 
 async function _delete(id) {
