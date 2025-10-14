3e893e4685afeaa3c5a7346e87e84eae8cba79c5
Zhypherion <luckybow1232@gmail.com>
2025-10-14 17:29:45 +0800

    Testing

--- positions/position.model.js
+++ positions/position.model.js
@@ -1,45 +1,45 @@
 // file: positions/position.model.js
-module.exports = (sequelize, DataTypes) => {
-  const Position = sequelize.define('Position', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: false },
-    status: { type: DataTypes.STRING, allowNull: false, defaultValue: 'Active' },
-    departmentId: { type: DataTypes.INTEGER, allowNull: true },
-    hierarchyLevel: { 
-      type: DataTypes.ENUM('Worker', 'Supervisor', 'Manager'),
-      allowNull: false,
-      defaultValue: 'Worker'
-    }
-  });
-
-Position.associate = (models) => {
-    Position.belongsTo(models.Department, {
-      foreignKey: 'departmentId',
-      as: 'department'
-    });
-  };
-  return Position;
-};
-
-
-
-
-// module.exports = (sequelize) => {
-//   const { DataTypes } = require('sequelize');
-
+// module.exports = (sequelize, DataTypes) => {
 //   const Position = sequelize.define('Position', {
+//     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 //     name: { type: DataTypes.STRING, allowNull: false },
 //     status: { type: DataTypes.STRING, allowNull: false, defaultValue: 'Active' },
-//     hierarchyLevel: { type: DataTypes.STRING, allowNull: false },
-//     departmentId: { type: DataTypes.INTEGER, allowNull: true }
+//     departmentId: { type: DataTypes.INTEGER, allowNull: true },
+//     hierarchyLevel: { 
+//       type: DataTypes.ENUM('Worker', 'Supervisor', 'Manager'),
+//       allowNull: false,
+//       defaultValue: 'Worker'
+//     }
 //   });
 
-//   Position.associate = (models) => {
+// Position.associate = (models) => {
 //     Position.belongsTo(models.Department, {
 //       foreignKey: 'departmentId',
 //       as: 'department'
 //     });
 //   };
-
 //   return Position;
 // };
+
+
+
+
+module.exports = (sequelize) => {
+  const { DataTypes } = require('sequelize');
+
+  const Position = sequelize.define('Position', {
+    name: { type: DataTypes.STRING, allowNull: false },
+    status: { type: DataTypes.STRING, allowNull: false, defaultValue: 'Active' },
+    hierarchyLevel: { type: DataTypes.STRING, allowNull: false },
+    departmentId: { type: DataTypes.INTEGER, allowNull: true }
+  });
+
+  Position.associate = (models) => {
+    Position.belongsTo(models.Department, {
+      foreignKey: 'departmentId',
+      as: 'department'
+    });
+  };
+
+  return Position;
+};
